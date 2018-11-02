a2979e6ef5f7c19fc3dd8eda36686985600c40fb
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-11-02 16:33:25 +0100

    added Migrate

--- manage.py
+++ manage.py
@@ -1,2 +1,14 @@
 from flask_scripts import Manager
+import os
+from flask_migrate import Migrate, MigrateCommand
+from src import create_app
+from src.db import db
 
+app = create_app(config_name=os.getenv('APP_SETTINGS'))
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+	manager.run()
